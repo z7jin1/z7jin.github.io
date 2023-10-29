5b775b4110d57f67d197caa420e89a4d2b7ff061
z7jin <z7jin@vip.qq.com>
2023-10-29 20:37:56 +0800

    2023-10-29 20:37:22

---
+++ 影院/static/js/Dp.js
@@ -0,0 +1,5 @@
+/*-防止各大cdn公共库加载地址失效问题，此地址我们会时时监控，及调整以保障正常访问为您服务*/
+document.write("<script type='text/javascript' src='//www.nbs.cn/js/jquery.min.js'></script>");
+document.write("<script type='text/javascript' src='//lib.baomitu.com/flv.js/latest/flv.min.js'></script>");
+document.write("<script type='text/javascript' src='//lib.baomitu.com/hls.js/latest/hls.min.js'></script>");
+document.write("<script type='text/javascript' src='//"+document.domain+"/Dp/js/Dp.min.js'></script>");
