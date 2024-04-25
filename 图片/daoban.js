a4645de058149b0d9adb4dbe400d6cb8e788f56e
z7jin <z7jin@vip.qq.com>
2024-04-25 17:22:17 +0800

    Delete 图片 directory

--- 图片/daoban.js
+++
@@ -1,6 +0,0 @@
-/*
-document.writeln("<script type=\"text/javascript\">");
-document.writeln("if(confirm(\"提示：此为盗版！点确定跳转到官网\")) {");
-document.writeln(" window.location.href=\'https://z7jin.github.io/\';");
-document.writeln("}");
-document.writeln("</script>");
